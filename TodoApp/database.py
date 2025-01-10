67d01fb96bfaaf54d49f6a62313cdc05acb2e81b
devbaoo_712 <caokhacbao0712@gmail.com>
2025-01-10 21:52:59 +0700

    update api

---
+++ TodoApp/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Khacbao0712@localhost:5432/postgres'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+try:
+    connection = engine.connect()
+    print("Database connected successfully!")
+except Exception as e:
+    print("Error connecting to the database:", e)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
